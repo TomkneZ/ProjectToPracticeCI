f78339605d890efdfad500924785779b5855dac9
TomkneZ <t.zabenko@itransition.com>
2021-07-06 23:14:43 +0300

    Added missing folders

---
+++ EducationalSystem.WebAPI/Options/AuthOptions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace EducationalSystem.WebAPI.Options
+{
+    public class AuthOptions
+    {
+        public const string ISSUER = "AuthServer";
+        public const string AUDIENCE = "AuthClient";
+        const string KEY = "supersecret_secretkey!123";
+        public const int LIFETIME = 1;
+        public static SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
+        }
+    }
+}
