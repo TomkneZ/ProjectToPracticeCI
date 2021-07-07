8b06611cda3928822683798a4b5405c04b216180
TomkneZ <t.zabenko@itransition.com>
2021-07-07 22:50:56 +0300

    remove unnecessary nuget packages

--- EducationalSystem.WebAPI/Filters/AuthorizationFilter.cs
+++ EducationalSystem.WebAPI/Filters/AuthorizationFilter.cs
@@ -1,10 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
-using System.Web.Http;
+using System;
 
 namespace EducationalSystem.WebAPI.Filters
 {
-    public class AuthorizationFilter : AuthorizeAttribute, IAuthorizationFilter
+    public class AuthorizationFilter : Attribute, IAuthorizationFilter
     {
         public void OnAuthorization(AuthorizationFilterContext filterContext)
         {
